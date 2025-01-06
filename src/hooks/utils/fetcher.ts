266e512d8315230d96e3fc964013bd346e9db1c8
wwbrdle <ww.brdle@gmail.com>
2025-01-05 19:18:16 -0800

    Changed BASE_URL

--- src/hooks/utils/fetcher.ts
+++ src/hooks/utils/fetcher.ts
@@ -1,5 +1,7 @@
 //export const BASE_URL = "http://localhost:8080";
-export const BASE_URL = "http://35.93.65.240:8080";
+//export const BASE_URL = "http://35.93.65.240:8080";
+export const BASE_URL = "http://ec2-35-93-65-240.us-west-2.compute.amazonaws.com:8080";
+
 
 export interface DefaultResponse<T> {
   success: boolean;
