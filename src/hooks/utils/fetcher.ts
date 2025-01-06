189e47947152ba0a3cea2d30081e7f1439f966cf
wwbrdle <ww.brdle@gmail.com>
2025-01-05 19:24:51 -0800

    test

--- src/hooks/utils/fetcher.ts
+++ src/hooks/utils/fetcher.ts
@@ -1,6 +1,6 @@
 //export const BASE_URL = "http://localhost:8080";
 //export const BASE_URL = "http://35.93.65.240:8080";
-export const BASE_URL = "http://ec2-35-93-65-240.us-west-2.compute.amazonaws.com:8080";
+export const BASE_URL = "https://ec2-35-93-65-240.us-west-2.compute.amazonaws.com:8080";
 
 
 export interface DefaultResponse<T> {
