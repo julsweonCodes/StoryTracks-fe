475f3bac7631d8261ea2baf739c732a9bdabf926
gyeongyeon <gjeon0321@gmail.com>
2025-01-16 00:19:54 +0900

    fix: card image background color & open drawer rounded

--- src/components/common/card.tsx
+++ src/components/common/card.tsx
@@ -17,20 +17,20 @@ export default function Card({ id, title, description, src }: Props) {
 
   return (
     <div className="flex flex-col gap-3" onClick={handleClick}>
-      <div className="flex h-[310px] w-full items-center justify-center overflow-hidden rounded-xl">
+      <div className="flex h-[310px] w-full items-center justify-center overflow-hidden rounded-xl bg-[#333333]">
         <Image
           src={src}
           width={320}
           height={320}
           alt={title}
-          className="w-full rounded-xl"
+          className="w-full"
           style={{
             objectFit: "cover",
             objectPosition: "center",
           }}
         />
       </div>
-      <div>???
+      <div>
         <h3 className="text-[14px] font-medium text-white-primary">{title}</h3>
         <p className="line-clamp-2 text-[14px] text-[#717375]">{description}</p>
       </div>
