723d486358850dda7bebb286317364524ace2194
wwbrdle <ww.brdle@gmail.com>
2025-01-15 18:06:14 -0800

    Upgraded dev environment 3

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig = {
   images: {
     unoptimized: true, // 이미지 최적화 비활성화
   },
+  env: {
+      BASE_URL: process.env.REACT_APP_BASE_URL || "http://localhost:8080/api",
+    },
 };
 
 export default nextConfig;
